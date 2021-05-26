775fa92c3e43eae27ded3354e47f1a67fb9f51b1
Paulo Bruno <paulo.bf111@gmail.com>
2021-05-25 22:39:20 -0300

    SnackBar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Design/drawer.dart';
+import 'Design/snackbar.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: DrawerPage(),
+      home: SnackBarPage(),
     );
   }
 }
