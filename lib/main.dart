d0f2460846f970b2d0b26784fc5c158becf4e416
Paulo Bruno <paulo.bf111@gmail.com>
2021-05-25 22:25:08 -0300

    main updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'Animation/animatedOpacity.dart';
+import 'Design/drawer.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: AnimatedOpacityPage(),
+      home: DrawerPage(),
     );
   }
 }
